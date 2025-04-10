8c0df047df00e583c255510635a601b7e050afd5
ikhwan <ikhwan0059@gmail.com>
2025-04-10 18:28:35 +0800

    create repository package and create UserRepository class

--- userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -1,7 +1,20 @@
 package com.fitness.userservice.service;
 
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.model.User;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    public UserResponse register(@Valid RegisterRequest request) {
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+
+
+    }
 }
