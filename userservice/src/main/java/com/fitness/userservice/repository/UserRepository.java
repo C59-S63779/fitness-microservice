1edf4567f4fb7642569006fd5a93155e33de4b41
ikhwan <ikhwan0059@gmail.com>
2025-04-20 18:31:56 +0800

    add anything in second last part in video part 6

--- userservice/src/main/java/com/fitness/userservice/repository/UserRepository.java
+++ userservice/src/main/java/com/fitness/userservice/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.fitness.userservice.repository;
 import com.fitness.userservice.dto.RegisterRequest;
 import com.fitness.userservice.model.User;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,8 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, String> {
 
     boolean existsByEmail(String email);
+
+    Boolean existsByKeycloakId(String userId);
+
+    User findByEmail(String email);
 }
--- userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -19,7 +19,17 @@ public class UserService {
     public UserResponse register(@Valid RegisterRequest request) {
 
         if (repository.existsByEmail(request.getEmail())) {
-            throw new RuntimeException("Email already exits");
+            User existingUser = repository.findByEmail((request.getEmail()));
+            UserResponse userResponse = new UserResponse();
+            userResponse.setId(existingUser.getId());
+            userResponse.setKeycloakId(existingUser.getKeycloakId());
+            userResponse.setPassword(existingUser.getPassword());
+            userResponse.setEmail(existingUser.getEmail());
+            userResponse.setFirstName(existingUser.getFirstName());
+            userResponse.setLastName(existingUser.getLastName());
+            userResponse.setCreatedAt(existingUser.getCreatedAt());
+            userResponse.setUpdatedAt(existingUser.getUpdatedAt());
+            return userResponse;
         }
 
         User user = new User();
@@ -30,6 +40,7 @@ public class UserService {
 
         User savedUser = repository.save(user);
         UserResponse userResponse = new UserResponse();
+        userResponse.setKeycloakId(savedUser.getKeycloakId());
         userResponse.setId(savedUser.getId());
         userResponse.setPassword(savedUser.getPassword());
         userResponse.setEmail(savedUser.getEmail());
@@ -59,6 +70,6 @@ public class UserService {
 
     public Boolean exitsByUserId(String userId) {
         log.info("Calling User Validation API for userId: {}", userId);
-        return repository.existsById(userId);
+        return repository.existsByKeycloakId(userId);
     }
 }
