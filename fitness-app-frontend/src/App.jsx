016ac72b79792826665eab0bc7ef7e4ff55cac27
ikhwan <ikhwan0059@gmail.com>
2025-04-30 17:57:48 +0800

    install  material ui

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,9 +1,16 @@
 /** @format */
 
 // import './App.css'
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router";
 
 function App() {
-  return <></>;
+  return <Router>button</Router>;
 }
 
 export default App;
