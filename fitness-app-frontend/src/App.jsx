9f894f0245dda1cfc15b8920752db7f8e23ac3a1
ikhwan <ikhwan0059@gmail.com>
2025-05-02 04:05:36 +0800

    secure OAuth2 authentication in frontend video part 7

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,6 +1,10 @@
 /** @format */
 
 import { Button } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { setCredentials } from "./store/authSlice";
+import { useDispatch } from "react-redux";
+import { AuthContext } from "react-oauth2-code-pkce";
 import {
   BrowserRouter as Router,
   Navigate,
@@ -10,11 +14,39 @@ import {
 } from "react-router";
 
 function App() {
+  const { token, tokenData, logIn, logOut, isAuthenticated } =
+    useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setCredentials({ token, user: tokenData }));
+      setAuthReady(true);
+    }
+  }, [token, tokenData, dispatch]);
+
   return (
     <Router>
-      <Button variant="contained" color="#dc004e">
-        LOGIN
-      </Button>
+      <div className="container">
+        {!token ? (
+          <div className="login-container">
+            <h1>Fitness App</h1>
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: "#dc004e" }}
+              onClick={() => logIn()}
+            >
+              LOGIN
+            </Button>
+          </div>
+        ) : (
+          <div className="authenticated-container">
+            <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+            <pre>{JSON.stringify(token, null, 2)}</pre>
+          </div>
+        )}
+      </div>
     </Router>
   );
 }
